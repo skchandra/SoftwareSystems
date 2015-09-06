f3a63fc85a1adac28d2022ee0cfbd2db0e6fe66b
skchandra <shivali.chandra@students.olin.edu>
2015-09-06 15:16:44 -0400

    hello.c program from ThinkOS problems

---
+++ hw01/hello.c
@@ -0,0 +1,15 @@
+/* Basic hello world program from
+ThinkOS.
+*/
+
+#include <stdio.h>
+
+int main() {
+	int x = 5;
+	printf("%i\n", x);
+	int y = x+1;
+}
+
+/*
+12. -02 gives 'unrecognized command line option '-02''
+*/

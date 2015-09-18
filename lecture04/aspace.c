b3bf94b3c617ebe0291e90a7c37f2e11553583b4
skchandra <shivali.chandra@students.olin.edu>
2015-09-18 01:20:52 -0400

    thinkOS chapter 3 questions

--- lecture04/aspace.c
+++ lecture04/aspace.c
@@ -14,11 +14,15 @@ int main ()
 {
     int local = 5;
     void *p = malloc(128);
+    void *c = malloc(128);
+    int otherLocal = 7;
 
     printf ("Address of main is %p\n", main);
     printf ("Address of global is %p\n", &global);
     printf ("Address of local is %p\n", &local);
     printf ("Address of p is %p\n", p);
+    printf ("Address of c is %p\n", c);
+    printf ("Address of otherLocal is %p\n", &otherLocal);
     
     return 0;
 }

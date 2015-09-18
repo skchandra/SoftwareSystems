9d7291f7dd7b9713c9def94be65a85ad4a36a772
skchandra <shivali.chandra@students.olin.edu>
2015-09-17 23:31:15 -0400

    Regex code to find tracks

--- hw02/find_track.c
+++ hw02/find_track.c
@@ -3,11 +3,13 @@
 Copyright 2014 Allen Downey
 License: Creative Commons Attribution-ShareAlike 3.0
 
+Code added by Shivali 9/16/15
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <regex.h>
 
 #define NUM_TRACKS 5
 
@@ -19,26 +21,32 @@ char tracks[][80] = {
     "Flamenco Sketches"
 };
 
+void find_track_regex(char pattern[])
+{
+    regex_t rt;
+    if (regcomp(&re, pattern, REG_EXTENDED|REG_ICASE) != 0) //Only returns if case is correct
+		return  0;
+	int rf = regcomp(&re, pattern, REG_EXTENDED|REG_ICASE);
+	char buf[80]; //buffer character array
+	if (rf != 0) { //print error if any
+		regerror(rf, &rt, buf, 80);
+		printf("recomp failure: '%s'\n", buf);
+		exit(EXIT_FAILURE);
+	}
+	int i;
+	for (i = 0; i < NUM_TRACKS; i++) { //print tracks
+		int state = regexec(&rt, tracks[i], (size_t) 0, NULL, 0);
+	}
+}
 
-// Finds all tracks that contain the given string.
-//
-// Prints track number and title.
-void find_track(char search_for[])
+/*void find_track(char search_for[])
 {
     int i;
     for (i=0; i<NUM_TRACKS; i++) {
-	if (strstr(tracks[i], search_for)) {
-	    printf("Track %i: '%s'\n", i, tracks[i]);
-	}
+		if (strstr(tracks[i], search_for)) {
+			printf("Track %i: '%s'\n", i, tracks[i]);
+		}
     }
-}
-
-// Finds all tracks that match the given pattern.
-//
-// Prints track number and title.
-void find_track_regex(char pattern[])
-{
-    // TODO: fill this in
 }
 
 // Truncates the string at the first newline, if there is one.
@@ -46,9 +54,9 @@ void rstrip(char s[])
 {
     char *ptr = strchr(s, '\n');
     if (ptr) {
-	*ptr = '\0';
+		*ptr = '\0';
     }
-}
+}*/
 
 int main (int argc, char *argv[])
 {
@@ -57,10 +65,12 @@ int main (int argc, char *argv[])
     /* take input from the user and search */
     printf("Search for: ");
     fgets(search_for, 80, stdin);
-    rstrip(search_for);
+    //rstrip(search_for);
 
-    find_track(search_for);
-    //find_track_regex(search_for);
+    //find_track(search_for);
+    find_track_regex(search_for);
 
     return 0;
 }
+
+

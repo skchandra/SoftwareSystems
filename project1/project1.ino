22f9acd09fe5acec8be72bf41d6ccb92ba975d04
skchandra <shivali.chandra@students.olin.edu>
2015-09-22 11:46:20 -0400

    Project 1 code

---
+++ project1/project1.ino
@@ -0,0 +1,73 @@
+//Sassy Salamanders Project 1 Code 
+//Sine out w/ 40kHz sampling rate
+
+//Basic code from Amanda Ghassaei
+//http://www.instructables.com/id/Arduino-Audio-Output/
+//Sept 2012
+
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+*/
+
+byte sine[] = {127, 134, 142, 150, 158, 166, 173, 181, 188, 195, 201, 207, 213, 219, 224, 229, 234, 238, 241, 245, 247, 250, 251, 252, 253, 254, 253, 252, 251, 250, 247, 245, 241, 238, 234, 229, 224, 219, 213, 207, 201, 195, 188, 181, 173, 166, 158, 150, 142, 134, 127, 119, 111, 103, 95, 87, 80, 72, 65, 58, 52, 46, 40, 34, 29, 24, 19, 15, 12, 8, 6, 3, 2, 1, 0, 0, 0, 1, 2, 3, 6, 8, 12, 15, 19, 24, 29, 34, 40, 46, 52, 58, 65, 72, 80, 87, 95, 103, 111, 119,};
+byte saw[] = {
+    0x22, 0x44, 0x66, 0x88, 0xaa, 0xcc, 0xee, 0x110, 0x132, 0x154,
+    0x176, 0x198, 0x1ba, 0x1dc, 0x1fe, 0x220, 0x242, 0x264, 0x286, 0x2a8,
+    0x2ca, 0x2ec, 0x30e, 0x330, 0x352, 0x374, 0x396, 0x3b8, 0x3da, 0x3fc,
+    0x41e, 0x440, 0x462, 0x484, 0x4a6, 0x4c8, 0x4ea, 0x50c, 0x52e, 0x550,
+    0x572, 0x594, 0x5b6, 0x5d8, 0x5fa, 0x61c, 0x63e, 0x660, 0x682, 0x6a4,
+    0x6c6, 0x6e8, 0x70a, 0x72c, 0x74e, 0x770, 0x792, 0x7b4, 0x7d6, 0x7f8,
+    0x81a, 0x83c, 0x85e, 0x880, 0x8a2, 0x8c4, 0x8e6, 0x908, 0x92a, 0x94c,
+    0x96e, 0x990, 0x9b2, 0x9d4, 0x9f6, 0xa18, 0xa3a, 0xa5c, 0xa7e, 0xaa0,
+    0xac2, 0xae4, 0xb06, 0xb28, 0xb4a, 0xb6c, 0xb8e, 0xbb0, 0xbd2, 0xbf4,
+    0xc16, 0xc38, 0xc5a, 0xc7c, 0xc9e, 0xcc0, 0xce2, 0xd04, 0xd26, 0xd48,
+    0xd6a, 0xd8c, 0xdae, 0xdd0, 0xdf2, 0xe14, 0xe36, 0xe58, 0xe7a, 0xe9c,
+    0xebe, 0xee0, 0xf02, 0xf24, 0xf46, 0xf68, 0xf8a, 0xfac, 0xfce, 0xff0
+  };
+int t = 0;//time
+
+void setup(){
+  //set digital pins 0-7 as outputs
+  for (int i=0;i<8;i++){
+    pinMode(i,OUTPUT);
+  }
+  
+  cli();//disable interrupts
+  //set timer0 interrupt at 40kHz
+  TCCR0A = 0;// set entire TCCR0A register to 0
+  TCCR0B = 0;// same for TCCR0B
+  TCNT0  = 0;//initialize counter value to 0
+  // set compare match register for 40khz increments
+  OCR0A = 49;// = (16*10^6) / (40000*8) - 1 (must be <256)
+  // turn on CTC mode
+  TCCR0A |= (1 << WGM01);
+  // Set CS11 bit for 8 prescaler
+  TCCR0B |= (1 << CS11); 
+  // enable timer compare interrupt
+  TIMSK0 |= (1 << OCIE0A);
+  sei();//enable interrupts
+  
+}
+
+
+ISR(TIMER0_COMPA_vect){ //40kHz interrupt routine
+  if (t < 51) {
+    PORTD = sine[t];//send sine wave to DAC, centered around (127/255)*5 = 2.5V
+  } else if (t > 50 && t < 99) {
+    PORTD = sine[t];
+  } else {
+    t = 0;
+  }
+  t++;//increment t
+}
+
+void loop(){
+  PORTD=byte(127+127*sin(t/100));
+}
+
+
+
